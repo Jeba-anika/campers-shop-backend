d707fd25125f2ce2139402b043c83d878b7efc55
Jeba Anika <anikanur98@gmail.com>
2024-07-10 12:48:30 +0600

    feat: add product

--- src/app/modules/product/product.router.ts
+++ src/app/modules/product/product.router.ts
@@ -1,14 +1,18 @@
 import express from 'express'
 import validateRequest from '../../middlewares/validateRequest'
 import { ProductController } from './product.controller'
+import { convertReq, parser } from './product.utils'
 import { ProductValidations } from './product.validation'
 const router = express.Router()
 
 router.post(
   '/',
+  parser.array('images', 10),
+  convertReq,
   validateRequest(ProductValidations.createProductValidation),
   ProductController.createProduct,
 )
+
 // router.get('/:id', CarController.getSingleCar)
 // router.put(
 //   '/return',
--- src/app/modules/product/product.utils.ts
+++ src/app/modules/product/product.utils.ts
@@ -1,20 +1,47 @@
-export const calcTotalDuration = (startTime: string, endTime: string) => {
-  const startHourAndMin = startTime.split(':')
-  const endHourAndMin = endTime.split(':')
-  const startHour = Number(startHourAndMin[0])
-  const startMin = Number(startHourAndMin[1])
-  const endHour = Number(endHourAndMin[0])
-  const endMin = Number(endHourAndMin[1])
-  let timeDiff = 0
-  const hourDiff = endHour - startHour
-  if (endMin - startMin === 0) {
-    timeDiff = hourDiff
-  } else if (startMin > endMin) {
-    const minDiff = startMin - endMin
-    timeDiff = hourDiff - 1 + minDiff / 60
-  } else if (startMin < endMin) {
-    const minDiff = endMin - startMin
-    timeDiff = hourDiff + Number((minDiff / 60).toFixed(2))
-  }
-  return timeDiff
-}
+import { v2 as cloudinary } from 'cloudinary'
+import { NextFunction, Request, Response } from 'express'
+import httpStatus from 'http-status'
+import multer from 'multer'
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import config from '../../config'
+import AppError from '../../errors/AppError'
+import catchAsync from '../../utils/catchAsync'
+cloudinary.config({
+  cloud_name: config.cloudinary_cloud_name,
+  api_key: config.cloudinary_api_key,
+  api_secret: config.cloudinary_api_secret,
+})
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'uploads', // folder in cloudinary
+    format: async (req, file) => 'png', // supports promises as well
+    public_id: (req, file) => file.originalname,
+  },
+})
+
+export const parser = multer({ storage })
+
+export const convertReq = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (req.files) {
+      const data = JSON.parse(req.body.data)
+      const newData = {
+        ...data,
+        productImagesLink: req.files.map((file) => ({
+          altText: file?.filename,
+          url: file?.path,
+        })),
+      }
+      req.body = newData
+      next()
+    } else {
+      throw new AppError(
+        httpStatus.NOT_ACCEPTABLE,
+        'Please upload at least one image',
+      )
+    }
+  },
+)
+export default cloudinary
